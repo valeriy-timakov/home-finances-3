7ddac90a10b01a62432ff995cd75c351b921ef7b
Valerii <valtimakov@gmail.com>
2025-07-16 08:40:09 +0300

    fix

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [PrismaModule, TransactionsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
