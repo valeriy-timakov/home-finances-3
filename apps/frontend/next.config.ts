a9412ea9b7cb69c9bb9e9d3ad52561889e98d611
Valerii <valtimakov@gmail.com>
2025-07-18 10:45:53 +0300

    fixes and proxy

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
+console.log("=== process.env variables in next.config.ts ===");
+for (const [key, value] of Object.entries(process.env)) {
+  console.log(`${key} = ${value}`);
+}
+console.log("=== END process.env -dump- ===");
+
+const apiUrl = process.env.API_URL;
+
 const nextConfig: NextConfig = {
-    /* config options here */
+    async rewrites() {
+        console.log(`rewrites to ${apiUrl}`);
+        return [
+            {
+                source: '/transactions/:path*',
+                destination: `${apiUrl}/transactions/:path*`,
+            },
+        ];
+    },
+    async proxy() {
+        return [
+            {
+                source: '/:path*',
+                destination: `${apiUrl}/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
